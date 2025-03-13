5d61d9831f3e52d0b2fa8de17b3e53b5b67d1801
DmitryBr32 <ipatdmitry@gmail.com>
2025-03-13 23:58:11 +0300

    fix:исправил ошибку роутинга на сервере

--- server/src/routes/recipe.routes.js
+++ server/src/routes/recipe.routes.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const RecipeController = require("../controllers/Recipe.controller.js");
 const verifyAccessToken = require("../middleware/verifyAccessToken.js");
 
-router.get("/", RecipeController.getIngredients);
-router.get("/:id", RecipeController.getIngredient);
-router.post("/", RecipeController.addIngredient);
-router.put("/:id", verifyAccessToken, RecipeController.updateIngredient);
-router.delete("/:id", verifyAccessToken, RecipeController.removeIngredient);
+router.get("/", RecipeController.getRecipes);
+router.get("/:id", RecipeController.getRecipe);
+router.post("/", RecipeController.addRecipe);
+router.put("/:id", verifyAccessToken, RecipeController.updateRecipe);
+router.delete("/:id", verifyAccessToken, RecipeController.removeRecipe);
 
 module.exports = router;
