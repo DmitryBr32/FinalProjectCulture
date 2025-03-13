32751758ff9e007cc92ce3ee8ef823e254afcc67
IshimyI <ivan.borisenko.2003@mail.ru>
2025-03-13 17:19:07 +0300

    merge culture/10/feat/server/entities/Bar

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -3,7 +3,7 @@ const authRoutes = require("./auth.routes");
 const shopRoutes = require("./shop.routes");
 const basketsRoutes = require("./baskets.routes");
 const ingredientRoutes = require("./ingredient.routes");
-const stockRoutes = require("./stock.routes")
+const stockRoutes = require("./stock.routes");
 const formatResponse = require("../utils/formatResponse");
 
 router.use("/auth", authRoutes);
