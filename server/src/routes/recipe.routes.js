12163d57ddba6ff31af4404222c7b84b1d3add97
DmitryBr32 <ipatdmitry@gmail.com>
2025-03-15 01:23:14 +0300

    feat:добавил эндпоинт для получения рецептов

--- server/src/routes/recipe.routes.js
+++ server/src/routes/recipe.routes.js
@@ -4,6 +4,12 @@ const verifyAccessToken = require("../middleware/verifyAccessToken.js");
 
 router.get("/", RecipeController.getRecipes);
 router.get("/:id", RecipeController.getRecipe);
+router.post("/getRecipesByIngr", RecipeController.getRecipesByIngr);
+router.post(
+  "/getRecipesBySeveralIngrs",
+  RecipeController.getRecipesBySeveralIngrs
+);
+router.post("/getByTitle", RecipeController.getRecipeByTitle);
 router.post("/", RecipeController.addRecipe);
 router.put("/:id", verifyAccessToken, RecipeController.updateRecipe);
 router.delete("/:id", verifyAccessToken, RecipeController.removeRecipe);
