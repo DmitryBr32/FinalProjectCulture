7c9a9208c6aa7ef3582082390d47c7204ecfd1b9
Maria Rodionova <mbelovaa15@gmail.com>
2025-03-14 22:38:07 +0300

    feat/ модалка

--- client/src/features/ui/JournalForm/JournalForm.tsx
+++ client/src/features/ui/JournalForm/JournalForm.tsx
@@ -1,30 +1,55 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 import styles from "./JournalForm.module.css";
+import ModalRecipe from "@/widgets/ModalRecipe/ModalRecipe";
 
   const recipe = 
   [
     {
       "id": 1,
       "title": "Мохито",
+      "description": "Описание рецепта Мохито",
+      "ingredients": ["Сахар", "Молоко", "Вода"],
+      "instructions": "Смешать все ингредиенты и добавить воду по вкусу",
       "image": "1.jpg"
     },
     {
       "id": 2,
       "title": "Палома",
+      "description": "Описание рецепта Палома",
+      "ingredients": ["Сахар", "Молоко", "Вода"],
+      "instructions": "Смешать все ингредиенты и добавить воду по вкусу",
       "image": "2.jpg"
     },
     {
       "id": 3,
       "title": "Б-52",
+      "description": "Описание рецепта Б-52",
+      "ingredients": ["Сахар", "Молоко", "Вода"],
+      "instructions": "Смешать все ингредиенты и добавить воду по вкусу",
       "image": "3.jpg"
     },
     {
       "id": 4,
       "title": "Зеленый дракон",
+      "description": "Описание рецепта Зеленый дракон",
+      "ingredients": ["Сахар", "Молоко", "Вода"],
+      "instructions": "Смешать все ингредиенты и добавить воду по вкусу",
       "image": "4.jpg"
     }
   ]
   export default function JournalForm(): JSX.Element {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  const openModal = (recipe) => {
+    setSelectedRecipe(recipe);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedRecipe(null);
+  };
     return (
       <div className={styles.container}>
       <h1 className={styles.title}>Рецепты</h1>
@@ -46,10 +71,13 @@ import styles from "./JournalForm.module.css";
               className={styles.recipeImage}
             />
             <h3 className={styles.recipeTitle}>{recipe.title}</h3>
-            <button className={styles.recipeButton}>Подробнее</button>
+            <button className={styles.recipeButton}  onClick={() => openModal(recipe)}>Подробнее</button>
           </div>
         ))}
       </div>
+      {isModalOpen && (
+        <ModalRecipe recipe={selectedRecipe} onClose={closeModal} />
+      )}
     </div>
     );
   }
--- client/src/pages/JournalPage/JournalPage.tsx
+++ client/src/pages/JournalPage/JournalPage.tsx
@@ -1,10 +1,7 @@
 import JournalForm from "@/features/ui/JournalForm/JournalForm";
-<<<<<<< HEAD
 import RecipeUserForm from "@/features/ui/PecipeUserForm/PecipeUserForm";
-=======
 import { JSX } from "react";
 
->>>>>>> 40429728f3338b7442951f27cb707e95d5dd5d20
 export function JournalPage(): JSX.Element {
   return (
     <div>
