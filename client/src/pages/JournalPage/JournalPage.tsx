146933892f8076b5c6ed8db7c5da440cb153291b
Maria Rodionova <mbelovaa15@gmail.com>
2025-03-14 09:49:08 +0300

    часть с напитками юзеров

--- client/src/pages/JournalPage/JournalPage.tsx
+++ client/src/pages/JournalPage/JournalPage.tsx
@@ -1,9 +1,10 @@
 import JournalForm from "@/features/ui/JournalForm/JournalForm";
-
+import RecipeUserForm from "@/features/ui/PecipeUserForm/PecipeUserForm";
 export function JournalPage(): JSX.Element {
   return (
     <div>
       <JournalForm />
+      <RecipeUserForm />
     </div>
   );
 }
