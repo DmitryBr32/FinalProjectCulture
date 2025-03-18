5fe347d93dd9101baef3fc1e330d7ebd00802b65
IshimyI <ivan.borisenko.2003@mail.ru>
2025-03-18 16:28:04 +0300

    culture/47/feet/Bar/recipes/new-widgets

--- client/src/widgets/CanBeAvailableCocktails/CanBeAvailableCocktails.tsx
+++ client/src/widgets/CanBeAvailableCocktails/CanBeAvailableCocktails.tsx
@@ -1,12 +1,10 @@
-import { useAppDispatch, useAppSelector } from "@/shared/hooks/reduxHook";
-import styles from "./AvailableCocktails.module.css";
-import { useEffect } from "react";
 import { getRecipesThunk } from "@/entities/recipe";
-import { getStockThunk } from "@/entities/stock";
+import { useAppDispatch, useAppSelector } from "@/shared/hooks/reduxHook";
+import { useEffect } from "react";
+import styles from "./CanBeAvailableCocktails.module.css";
 
-export default function AvailableCocktails() {
+export default function CanBeAvailableCocktails() {
   const recipes = useAppSelector((state) => state.recipes.recipes);
-  const stock = useAppSelector((state) => state.stock.stock);
   const user = useAppSelector((state) => state.user.user?.id);
 
   const dispatch = useAppDispatch();
@@ -17,21 +15,12 @@ export default function AvailableCocktails() {
 
   console.log(recipes);
 
-  const filteredRecipes = recipes;
-
-  // .filter((recipe) => {
-  //   const recipeTypes = recipe.Components.map((comp) => comp.ingredient.type);
-  //   return recipe.Components.some((comp) =>
-  //     recipeTypes.includes(comp.ingredient.type)
-  //   );
-  // });
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Доступные коктейли</h1>
       <div className={styles.recipesGrid}>
-        {Array.isArray(filteredRecipes) && filteredRecipes.length > 0 ? (
-          filteredRecipes.map((recipe) => (
+        {Array.isArray(recipes) && recipes.length > 0 ? (
+          recipes.map((recipe) => (
             <div key={recipe.id} className={styles.recipeCard}>
               <div className={styles.recipeHeader}>
                 <img
