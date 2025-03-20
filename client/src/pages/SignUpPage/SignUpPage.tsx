d74f4db4be4e53fed479176868da96171716bc00
IshimyI <ivan.borisenko.2003@mail.ru>
2025-03-20 16:27:29 +0300

    done auth culture/fix/styles/Bar/buttons

--- client/src/pages/SignUpPage/SignUpPage.tsx
+++ client/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,5 +1,18 @@
 import SignUpForm from "@/features/auth/ui/SignUpForm/SignUpForm";
+import { CLIENT_ROUTES } from "@/shared/enums/clientRoutes";
+import { useAppSelector } from "@/shared/hooks/reduxHook";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 export function SignUpPage() {
+  const user = useAppSelector((state) => state.user.user?.id);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate(CLIENT_ROUTES.MAIN);
+    }
+  }, [user, navigate]);
+
   return <SignUpForm />;
 }
